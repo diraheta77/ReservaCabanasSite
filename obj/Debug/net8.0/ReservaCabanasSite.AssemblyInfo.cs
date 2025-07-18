4c280353302979169ac16003f649dd722af7bce2
DiegoIraheta <diraheta@outlook.es>
2025-07-18 12:50:06 -0600

    chore: guardar cambios locales antes de pull

--- obj/Debug/net8.0/ReservaCabanasSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReservaCabanasSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservaCabanasSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9980665d8d97dcc3c33aaa9e9f8a51aa12ad64f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa197f870eeecfdb855877ef63025b7cc980dd7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservaCabanasSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservaCabanasSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
